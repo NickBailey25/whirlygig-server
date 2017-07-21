a8629986eada3841329628876bcc16fb5d61eb9e
Nick Bailey <nickrobertbailey@gmail.com>
2017-07-21 00:39:20 -0700

    Send Locations JSON

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app     = express();
-const player  = require('play-sound')(opts = {});
 
-app.get('/', (req, res) => {
-  player.play(`${__dirname}/damn-son.mp3`, (err) => { if (err) throw err });
-  res.send('Damn son, where\'d you find this?!');
+
+app.get('/locations', (req, res) => {
+  res.sendFile(__dirname + '/locations.json');
 });
 
-app.listen(3003, () => console.log('Listening on port 3003!'));
+app.listen(3003, () => console.log('get data at localhost:3003/locations'));
